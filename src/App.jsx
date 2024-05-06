10e7d59b69b1dd29759a0a9201fb8fb196d64d89
ovpn-dev <ovpn05@gmail.com>
2024-05-06 19:05:21 +0100

    oya deploy ijn

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Stack } from "@chakra-ui/react";
 import Navbar from "./components/nav";
 import HeroSection from "./components/hero";
-import audio from "./assets/Gegagedigedagedago.mp3";
 import NuggetsCard from "./components/nuggetsCard";
 
 function App() {
--- src/components/nuggetsCard.jsx
+++ src/components/nuggetsCard.jsx
@@ -19,26 +19,33 @@ import CrdImg7 from "../assets/cardImage7.jpg";
 export default function NuggetsCard() {
   return (
     <>
-      <SimpleGrid spacing={4} column={4}>
+      <SimpleGrid
+        spacing={4}
+        columns={{ base: 1, sm: 2, md: 2, lg: 3 }}
+        alignContent={"center"}
+        bg="GrayText"
+        p={5}
+      >
         <Card size={"lg"}>
-          <CardBody>
-            <Image src={CrdImg1} alt="Image description" />
-          </CardBody>
+          <Image src={CrdImg1} alt="Image description" />
         </Card>
         <Card size={"lg"}>
-          <CardBody>
-            <Image src={CrdImg2} alt="Image description" />
-          </CardBody>
+          <Image src={CrdImg2} alt="Image description" />
         </Card>
         <Card size={"lg"}>
-          <CardBody>
-            <Image src={CrdImg3} alt="Image description" />
-          </CardBody>
+          <Image src={CrdImg3} alt="Image description" />
         </Card>
         <Card size={"lg"}>
-          <CardBody>
-            <Image src={CrdImg4} alt="Image description" />
-          </CardBody>
+          <Image src={CrdImg4} alt="Image description" />
+        </Card>
+        <Card size={"lg"}>
+          <Image src={CrdImg5} alt="Image description" />
+        </Card>
+        <Card size={"lg"}>
+          <Image src={CrdImg6} alt="Image description" />
+        </Card>
+        <Card size={"lg"}>
+          <Image src={CrdImg7} alt="Image description" />
         </Card>
       </SimpleGrid>
     </>
